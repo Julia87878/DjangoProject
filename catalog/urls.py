d98beaf4c7b420b7d3ffec45bfb878293d26acb0
Julia Adonina <julie014@mail.ru>
2025-05-14 18:21:29 +0300

    templates

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 
+from catalog.apps import CatalogConfig
+
 from . import views
 
-app_name = "catalog"
+app_name = CatalogConfig.name
 
 urlpatterns = [
-    path("home/", views.home, name="home"),
+    path("products_list/", views.products_list, name="products_list"),
+    path("product_detail/<int:pk>", views.product_detail, name="product_detail"),
     path("contacts/", views.contacts, name="contacts"),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
 
+from .models import Product
 
-def home(request):
-    return render(request, "catalog/home.html")
+
+def products_list(request):
+    products = Product.objects.all()
+    context = {"products": products}
+    return render(request, "catalog/products_list.html", context)
+
+
+def product_detail(request, pk):
+    product = Product.objects.get(pk=pk)
+    context = {"product": product}
+    return render(request, "catalog/product_detail.html", context)
 
 
 def contacts(request):
