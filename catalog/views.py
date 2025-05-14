2919ab098d6d6fbe2b3961b2facc4efb930cd2f2
Julia87878 <uliaadonina19@gmail.com>
2025-05-14 21:20:27 +0300

    Update catalog/views.py
    Co-authored-by: painassasin <painassasin@icloud.com>

--- catalog/views.py
+++ catalog/views.py
@@ -10,7 +10,7 @@ def products_list(request):
 
 
 def product_detail(request, pk):
-    product = Product.objects.get(pk=pk)
+    product = get_object_or_404(Product, pk=pk)
     context = {"product": product}
     return render(request, "catalog/product_detail.html", context)
 
